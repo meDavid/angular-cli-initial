6c711e7cdc01049a09205041f88c12cae4c8558a
Ankur Sethi <contact@ankursethi.in>
2018-11-15 15:53:16 +0530

    Add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+
+export interface AppState {}
+
+@Injectable()
+export class AppStateService {
+  constructor(private store: Store<AppState>) {}
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +19,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AppStateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
